38339591e418d95e075360749a725dc7283841c7
Chathan Driehuys <cdriehuys@gmail.com>
2017-08-31 12:05:35 -0400

    Create chess board.

--- app/src/main/java/unc/comp433/chathan/eightqueens/MainActivity.java
+++ app/src/main/java/unc/comp433/chathan/eightqueens/MainActivity.java
@@ -1,13 +1,54 @@
 package unc.comp433.chathan.eightqueens;
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int DARK_SQUARE = Color.parseColor("#755000");
+    private static final int LIGHT_SQUARE = Color.parseColor("#edc674");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createBoard();
+    }
+
+    private void createBoard() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        int width = size.x;
+        int buttonSize = width / 8;
+
+        GridLayout board = (GridLayout) findViewById(R.id.chess_grid_layout);
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Button cell = new Button(getApplicationContext());
+                cell.setHeight(buttonSize);
+                cell.setWidth(buttonSize);
+
+                if ((row + col) % 2 == 0) {
+                    cell.setBackgroundColor(DARK_SQUARE);
+                } else {
+                    cell.setBackgroundColor(LIGHT_SQUARE);
+                }
+
+                GridLayout.LayoutParams cellParams = new GridLayout.LayoutParams();
+                cellParams.width = buttonSize;
+                cellParams.height = buttonSize;
+
+                board.addView(cell, cellParams);
+            }
+        }
     }
 }
